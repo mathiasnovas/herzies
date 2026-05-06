b78ea47114d8492071a622c98c707406c08907cd
Mathias Novas <mathias.novas@try.no>
2026-05-06 23:06:45 +0200

    Revert "Fix PKCE flow by using @supabase/ssr browser client"
    This reverts commit 2889f265841090f1d1a19c5bcfeb9e9e5ce7583f.

--- packages/web/src/lib/supabase.ts
+++ packages/web/src/lib/supabase.ts
@@ -1,8 +1,13 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
 
 export function createSupabaseClient() {
-	return createBrowserClient(
+	return createClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			auth: {
+				flowType: "pkce",
+			},
+		},
 	);
 }
